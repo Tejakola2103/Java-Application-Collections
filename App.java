9a81bd0d4aded005178750f642bed7a15e8697fe
TEJA KOLA <tejakola67@gmail.com>
2026-03-14 11:47:21 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.Ioc.Di.App._IOC_DI_App;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
