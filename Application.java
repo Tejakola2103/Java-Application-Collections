d5d19c7881821073af07b325c0fdcce0e98496dd
TEJA KOLA <tejakola67@gmail.com>
2026-03-14 11:46:14 +0530

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,13 @@
+package com.Ioc.DI.App.__IOC_DI_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
